c0c7d0a9c6307f8d544244582117eaa8c5bcf44c
TiBasic <98064464+TiBasic@users.noreply.github.com>
2025-08-12 20:16:01 -0500

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Portfolio/' // Use the exact repository name with a leading and trailing slash
+});
