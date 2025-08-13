e87bf7227810af5f1caec7beb0a6204dacdd36be
TiBasic <98064464+TiBasic@users.noreply.github.com>
2025-08-12 20:21:19 -0500

    fix image paths (pls work)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './About.css';
+import me from '../assets/me.jpg';
 
 function About() {
     return (
         <div className = "about-page">
             <h1>About Me :D</h1>
-            <img src='/src/assets/me.jpg' alt="Megan Campbell" className="profile-pic" />
+            <img src={me} alt="Megan Campbell" className="profile-pic" />
             <p> Description yeah</p>
         </div>
     );
--- src/projectsData.js
+++ src/projectsData.js
@@ -1,16 +1,20 @@
+import airporttracker from "/src/assets/airporttracker.png"
+
+
+
 export const projects = [
     {id: 1,
     title: "Airport Tracker",
     description: "Full stack web application to track and rate airport experiences with Node.JS, Express, and vanilla JavaScript",
     tags: ["node.js", "webdev", "frontend", "backend"],
     github: "https://github.com/mjc180501/AirportTracker",
-    image: "/src/assets/airporttracker.png"
+    image: "{airporttracker}"
     },
     {id: 2,
     title: "Personal Portfolio",
     description: "React.js powered personal portfolio website.",
     tags: ["react.js", "frontend", "webdev"],
     github: "https://github.com/mjc180501/AirportTracker",
-    image: "/src/assets/airporttracker.png"
+    image: "{airporttracker}"
     },
 ];
